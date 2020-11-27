523c96e9506ebce7b439dd041108832b61b14ee0
melissikiss <melissikiss@gmail.com>
2020-11-26 17:10:43 -0800

    deleted unnecessary comments

--- Develop/server.js
+++ Develop/server.js
@@ -24,12 +24,6 @@ app.listen(3001, () => {
 });
 
 
-// GET /notes should return the notes.html file. ( done )
-
-// GET / should return the index.html file. ( done )
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON. ( done )
-
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note
 // a unique id when it's saved (look into npm packages that could do this for you).
 

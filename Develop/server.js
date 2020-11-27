318b614f3c6b7486c0c0f5d83141e1f19f09b4c8
melissikiss <melissikiss@gmail.com>
2020-11-26 17:08:31 -0800

    added heroku

--- Develop/server.js
+++ Develop/server.js
@@ -17,7 +17,7 @@ app.get('/notes', (req, res) => {
 // reads the db.json file and returns all saved notes as JSON
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-});
+}); 
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
